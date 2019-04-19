2ee238c9a27af4fc208383cd9c50a37906f43968
Songqing Oscar Li <sqoscarli@gmail.com>
2019-04-18 21:42:32 -0700

    Update twitterToKafka.py

--- kafka/kafka_2.10-0.10.0.1/twitterToKafka.py
+++ kafka/kafka_2.10-0.10.0.1/twitterToKafka.py
@@ -4,10 +4,10 @@ from tweepy import Stream
 from tweepy import OAuthHandler
 
 
-consumer_key =  'EL9bP53IKTBWsmNVkXeBaTLH0'
-consumer_secret = 'URuSzNfTCFBq7nPyaSqAjqksECbD2vUFUMq2RfhYSjoloWPQbq'
-access_token = '1063944351724662784-MVTRTpJk2JdoFTOl3UeVt04orBjSUJ'
-access_token_secret =  'GgwSxgkQVrXfnsQXxbuon3CDmV9GERKz0H8EYfv3Db76p'
+consumer_key =  /
+consumer_secret = /
+access_token = /
+access_token_secret =  /
 
 
 class KafkaListener(StreamListener):
